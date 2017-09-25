4f1e6a82b946d6b474ef7cd4a25b0189a6fc317a
Ricardo Sander Lopes <ricardo.sander.lopes@gmail.com>
2017-09-24 23:06:09 -0300

    Utilizando e aprendendo os status do objeto na JPA.

--- src/main/java/br/com/ricardosander/financas/testes/TesteConta.java
+++ src/main/java/br/com/ricardosander/financas/testes/TesteConta.java
@@ -8,8 +8,9 @@ import javax.persistence.Persistence;
 
 public class TesteConta {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
+        //Novo objeto e sem id, então é Transient, ou seja, não gerenciado pela JPA.
         Conta conta = new Conta();
         conta.setAgencia("123");
         conta.setNumero("456");
@@ -20,7 +21,30 @@ public class TesteConta {
         EntityManager em = emf.createEntityManager();
 
         em.getTransaction().begin();
-        em.persist(conta);
+        em.persist(conta);//Passou a ser gerenciado pela JPA (Managed).
+        em.getTransaction().commit();
+
+        em.close();
+
+        //Conta deixa de ser gerenciada pela JPA pois o EntityManager que a criou/buscou foi fechado (Detached).
+        conta.setTitular("Ricardo Lopes");
+
+        em = emf.createEntityManager();
+
+        em.merge(conta);//Passou a ser gerenciada pela JPA (Managed).
+
+        em.getTransaction().begin();
+        em.getTransaction().commit();
+
+        em.close();
+        //Novamente, o objeto deixa de ser Managed e passa a ser Detached.
+
+        em = emf.createEntityManager();
+
+        conta = em.find(Conta.class, conta.getId());//Novamente, o objeto passa de Detached para Managed.
+
+        em.getTransaction().begin();
+        em.remove(conta);
         em.getTransaction().commit();
 
         em.close();
