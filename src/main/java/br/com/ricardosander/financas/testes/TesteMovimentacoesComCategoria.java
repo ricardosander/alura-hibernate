374e92fdd724452a0b3150fd33f180e9adcf902c
Ricardo Sander Lopes <ricardo.sander.lopes@gmail.com>
2017-10-01 17:22:23 -0300

    Criando testes para relação de movimentação com categoria.

---
+++ src/main/java/br/com/ricardosander/financas/testes/TesteMovimentacoesComCategoria.java
@@ -0,0 +1,60 @@
+package br.com.ricardosander.financas.testes;
+
+import br.com.ricardosander.financas.modelo.Categoria;
+import br.com.ricardosander.financas.modelo.Conta;
+import br.com.ricardosander.financas.modelo.Movimentacao;
+import br.com.ricardosander.financas.modelo.TipoMovimentacao;
+import br.com.ricardosander.financas.util.JPAUtil;
+
+import javax.persistence.EntityManager;
+import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.Calendar;
+
+public class TesteMovimentacoesComCategoria {
+
+    public static void main(String[] args) {
+
+        Categoria categoria1 = new Categoria("Viagem");
+        Categoria categoria2 = new Categoria("Negócios");
+
+        Conta conta = new Conta();
+        conta.setId(1);
+        conta.setNumero("123");
+        conta.setTitular("Ricardo");
+        conta.setAgencia("456");
+        conta.setBanco("Meu Banco");
+
+        Movimentacao movimentacao1 = new Movimentacao();
+        movimentacao1.setValor(new BigDecimal("100.0"));
+        movimentacao1.setTipo(TipoMovimentacao.Saida);
+        movimentacao1.setDescricao("Viagem ao RJ");
+        movimentacao1.setData(Calendar.getInstance());
+        movimentacao1.setConta(conta);
+        movimentacao1.setCategorias(Arrays.asList(categoria1, categoria2));
+
+        Movimentacao movimentacao2 = new Movimentacao();
+        movimentacao2.setValor(new BigDecimal("150.0"));
+        movimentacao2.setTipo(TipoMovimentacao.Saida);
+        movimentacao2.setDescricao("Viagem à SP");
+        movimentacao2.setData(Calendar.getInstance());
+        movimentacao2.setConta(conta);
+        movimentacao2.setCategorias(Arrays.asList(categoria1, categoria2));
+
+        EntityManager entityManager = JPAUtil.getEntityManager();
+
+        entityManager.getTransaction().begin();
+
+        entityManager.merge(conta);
+
+        entityManager.persist(categoria1);
+        entityManager.persist(categoria2);
+
+        entityManager.persist(movimentacao1);
+        entityManager.persist(movimentacao2);
+
+        entityManager.getTransaction().commit();
+
+        entityManager.close();
+    }
+}
