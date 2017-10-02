9f93fcd7a488ed859e0a10dd0e368ea2fe806642
Ricardo Sander Lopes <ricardo.sander.lopes@gmail.com>
2017-10-01 21:31:04 -0300

    Movendo para padrão DAO.

---
+++ src/main/java/br/com/ricardosander/financas/dao/ClienteDao.java
@@ -0,0 +1,26 @@
+package br.com.ricardosander.financas.dao;
+
+import br.com.ricardosander.financas.modelo.Cliente;
+
+import javax.persistence.EntityManager;
+
+public class ClienteDao {
+
+    private final EntityManager entityManager;
+
+    public ClienteDao(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    public void save(Cliente cliente) {
+        entityManager.getTransaction().begin();
+        entityManager.persist(cliente);
+        entityManager.getTransaction().commit();
+    }
+
+    @Override
+    protected void finalize() throws Throwable {
+        entityManager.close();
+    }
+
+}
--- src/main/java/br/com/ricardosander/financas/testes/TestaContaCliente.java
+++ src/main/java/br/com/ricardosander/financas/testes/TestaContaCliente.java
@@ -1,17 +1,18 @@
 package br.com.ricardosander.financas.testes;
 
+import br.com.ricardosander.financas.dao.ClienteDao;
 import br.com.ricardosander.financas.modelo.Cliente;
 import br.com.ricardosander.financas.modelo.Conta;
 import br.com.ricardosander.financas.util.JPAUtil;
-
-import javax.persistence.EntityManager;
 
 public class TestaContaCliente {
 
     public static void main(String[] args) {
 
+        ClienteDao clienteDao = new ClienteDao(JPAUtil.getEntityManager());
+
         Conta conta = new Conta();
-        conta.setId(1);
+        conta.setId(2);
 
         Cliente cliente = new Cliente();
         cliente.setEndereco("Rua Fulano, 123");
@@ -19,21 +20,6 @@ public class TestaContaCliente {
         cliente.setProfissao("Programador");
         cliente.setConta(conta);
 
-        //Cliente cliente2 = new Cliente();
-        //cliente2.setEndereco("Rua Fulano, 1234");
-        //cliente2.setNome("Ricardo 2");
-        //cliente2.setProfissao("Programador 2");
-        //cliente2.setConta(conta);
-
-        EntityManager em = JPAUtil.getEntityManager();
-
-        em.getTransaction().begin();
-
-        em.persist(cliente);
-        //em.persist(cliente2);
-
-        em.getTransaction().commit();
-
-        em.close();
+        clienteDao.save(cliente);
     }
 }
