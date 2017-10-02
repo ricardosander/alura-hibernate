32084a00b2fac664e1c95a7937fd149d94fbced9
Ricardo Sander Lopes <ricardo.sander.lopes@gmail.com>
2017-10-01 21:51:27 -0300

    Movendo para padrão DAO.

--- src/main/java/br/com/ricardosander/financas/dao/MovimentacaoDao.java
+++ src/main/java/br/com/ricardosander/financas/dao/MovimentacaoDao.java
@@ -1,8 +1,13 @@
 package br.com.ricardosander.financas.dao;
 
+import br.com.ricardosander.financas.modelo.Categoria;
+import br.com.ricardosander.financas.modelo.Conta;
 import br.com.ricardosander.financas.modelo.Movimentacao;
+import br.com.ricardosander.financas.modelo.TipoMovimentacao;
 
 import javax.persistence.EntityManager;
+import javax.persistence.Query;
+import java.util.List;
 
 public class MovimentacaoDao {
 
@@ -19,4 +24,16 @@ public class MovimentacaoDao {
         entityManager.getTransaction().commit();
     }
 
+    public List<Movimentacao> find(Conta conta, TipoMovimentacao tipoMovimentacao, Categoria categoria) {
+
+        String sQuery1 = " select m from Movimentacao m join m.categorias c where c = :pCategoria and m.conta = :pConta and m.tipo = :pTipo order by m.valor desc ";
+
+        Query query1 = entityManager.createQuery(sQuery1);
+
+        query1.setParameter("pConta", conta);
+        query1.setParameter("pTipo", tipoMovimentacao);
+        query1.setParameter("pCategoria", categoria);
+
+        return query1.getResultList();
+    }
 }
--- src/main/java/br/com/ricardosander/financas/testes/TesteJPQL.java
+++ src/main/java/br/com/ricardosander/financas/testes/TesteJPQL.java
@@ -1,5 +1,6 @@
 package br.com.ricardosander.financas.testes;
 
+import br.com.ricardosander.financas.dao.MovimentacaoDao;
 import br.com.ricardosander.financas.modelo.Categoria;
 import br.com.ricardosander.financas.modelo.Conta;
 import br.com.ricardosander.financas.modelo.Movimentacao;
@@ -7,29 +8,22 @@ import br.com.ricardosander.financas.modelo.TipoMovimentacao;
 import br.com.ricardosander.financas.util.JPAUtil;
 
 import javax.persistence.EntityManager;
-import javax.persistence.Query;
 import java.util.List;
 
 public class TesteJPQL {
 
     public static void main(String[] args) {
 
-        Conta conta1 = new Conta();
-        conta1.setId(1);
+        EntityManager em = JPAUtil.getEntityManager();
+        MovimentacaoDao movimentacaoDao = new MovimentacaoDao(em);
+
+        Conta conta = new Conta();
+        conta.setId(1);
 
         Categoria categoria = new Categoria();
         categoria.setId(1);
 
-        EntityManager em = JPAUtil.getEntityManager();
-
-        String sQuery1 = " select m from Movimentacao m join m.categorias c where c = :pCategoria and m.conta = :pConta and m.tipo = :pTipo order by m.valor desc ";
-
-        Query query1 = em.createQuery(sQuery1);
-        query1.setParameter("pConta", conta1);
-        query1.setParameter("pTipo", TipoMovimentacao.Saida);
-        query1.setParameter("pCategoria", categoria);
-
-        List<Movimentacao> resultList1 = query1.getResultList();
+        List<Movimentacao> resultList1 = movimentacaoDao.find(conta, TipoMovimentacao.Saida, categoria);
 
         resultList1.forEach(m -> {
             System.out.println("Conta: " + m.getConta().getId());
@@ -38,6 +32,8 @@ public class TesteJPQL {
             System.out.println("Tipo: " + m.getTipo());
             m.getCategorias().forEach(c -> System.out.println("Categoria:" + c.getNome()));
         });
+
+        em.close();
     }
 
 }
